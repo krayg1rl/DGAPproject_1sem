6080689e90037eab05437389fae955256dd41946
sk <sergeykorovichev@gmail.com>
2023-11-30 15:18:31 +0300

    added drawer object, draw functions

--- render.py
+++ render.py
@@ -2,6 +2,9 @@
 all drawing and displaying functions should be here
 """
 
+window_width = 1280
+window_height = 720
+
 RED = (255, 0, 0)
 BLACK = (0, 0, 0)
 
@@ -13,7 +16,10 @@ class Drawer:
     def update(self, drawables, screen):
         self.screen.fill(0, 0, 0)
         for drawable in drawables:
-            drawable.draw(screen)
+            drawable.draw(screen, self.center - pg.Vector2(window_width/2, window_height/2))
+
+    def get_center(self, center):
+        self.center = center
 
     def display(self):
         pg.display.update()
@@ -27,10 +33,11 @@ class Drawable:
 
 
 
-    def draw(self, surface):
+    def draw(self, surface, offset):
         if self.drawable_type == 0:
-            #surface.blit(self.obj.sprite, self.pos)
-            self.obj.draw(surface)
+            pos = pg.Vector2(self.obj.position.x, self.obj.position.y)
+            surface.blit(self.obj.sprite, pos + offset)
+            #self.obj.draw(surface)
         elif self.drawable_type == 1:
             surface.draw.rect(surface, RED, self.obj.position)
 
