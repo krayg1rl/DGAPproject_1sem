c73980eb9408295fa67245171d73859af6779268
Ваккер Богдан Сергеевич, lab01-executor <Vakker150217@gmail.com>
2023-12-05 14:51:33 +0300

    Movements of Main Character

--- main.py
+++ main.py
@@ -1,6 +1,52 @@
 """
 the main loop should be here
 """
-
+import pygame as pg
 WIDTH = 1280
 HEIGHT = 720
+FPS = 30
+
+pg.init()
+screen = pg.display.set_mode((WIDTH, HEIGHT))
+background = pg.transform.scale(pg.image.load("images/background.jpg"), (WIDTH, HEIGHT))
+clock = pg.time.Clock()
+finished = False
+
+Akey = 0
+Skey = 0
+Dkey = 0
+Wkey = 0
+Kspace = 0
+
+while not finished:
+    # TODO
+    #drawing of objects should be here
+
+    # Reaction to keys
+    clock.tick(FPS)
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            finished = True
+        elif event.type == pg.KEYDOWN:
+            if (event.key == pg.K_SPACE):
+                if (Kspace):
+                    Kspace = 0
+                else:
+                    Kspace = 1
+            if event.key == pg.K_a:
+                Akey = 1
+            if event.key == pg.K_s:
+                Skey = 1
+            if event.key == pg.K_d:
+                Dkey = 1
+            if event.key == pg.K_w:
+                Wkey = 1
+        elif event.type == pg.KEYUP:
+            if event.key == pg.K_a:
+                Akey = 0
+            if event.key == pg.K_s:
+                Skey = 0
+            if event.key == pg.K_d:
+                Dkey = 0
+            if event.key == pg.K_w:
+                Wkey = 0
--- object.py
+++ object.py
@@ -1,10 +1,10 @@
 import pygame as pg
 
 class Object:
-    def __init__(self):
+    def __init__(self, screen: pg.Surface):
         self.position = pg.Rect(0, 0, 0, 0)
         self.speed = pg.Vector2(0, 0)
-        #self.image
+        self.image =
 
     def draw(self):
         pass
@@ -23,7 +23,40 @@ class Main_character:
         #self.image = pg
 
     def move(self, objects, Akey, Wkey, Skey, Dkey):
-        pass
+        leftcrash = 0
+        rightcrash = 0
+        bottomcrash = 0
+        topcrash = 0
+        for obj in objects:
+            if ((self.position.top< obj.position.bottom) and (self.position.bottom > obj.position.top) and(self.position.left<obj.position.right) and (self.position.left>obj.position.centerx)):
+                leftcrash = True
+            if ((self.position.top< obj.position.bottom) and (self.position.bottom > obj.position.top) and(self.position.right>obj.position.left) and (self.position.right<obj.position.centerx)):
+                rightcrash = True
+            if((self.position.left<obj.position.right) and (self.position.right>obj.position.left) and (self.position.top< obj.position.bottom) and (self.position.top>obj.position.centery)):
+                topcrash = True
+            if((self.position.left<obj.position.right) and (self.position.right>obj.position.left) and (self.position.bottom> obj.position.top) and (self.position.bottom<obj.position.centery)):
+                bottomcrash = True
+
+        if(Akey and not Dkey):
+            if(leftcrash):
+                self.position.x=self.position.x + 2*self.speed.x
+            else:
+                self.position.x-=self.speed.x
+        elif(Dkey and not Akey):
+            if(rightcrash):
+                self.position.x = self.position.x - 2 * self.speed.x
+            else:
+                self.position.x+=self.speed.x
+        if(Wkey and not Skey):
+            if(topcrash):
+                self.position.y = self.position.y+2*self.speed.y
+            else:
+                self.position.y-=self.speed.y
+        elif(Skey and not Wkey):
+            if(bottomcrash):
+                self.position.y = self.position.y-2*self.speed.y
+            else:
+                self.position.y += self.speed.y
 
 
 
