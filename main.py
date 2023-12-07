624d15fd47fdb05d936eb6c868f89af01cd712bd
Daniil-Kharitonov <prostoharia@gmail.com>
2023-12-07 13:05:23 +0300

    added 'handle_events' function, which is aimed at managing keyboard(or mouse) events

--- main.py
+++ main.py
@@ -17,18 +17,54 @@ desk_image = pg.transform.scale(pg.image.load("pictures/Desk.png"), (300, 300))
 hero_image = pg.transform.scale(pg.image.load("pictures/prep1.png"), (80, 200))
 clock = pg.time.Clock()
 finished = False
-objects =[]
+objects = []
 Table = Object(screen, desk_image)
 objects.append(Table)
 
 hero = Main_character(screen, hero_image)
-hero.speed.y=30
-hero.speed.x=30
-Akey = 0
-Skey = 0
-Dkey = 0
-Wkey = 0
-Kspace = 0
+hero.speed.y = 30
+hero.speed.x = 30
+
+# keys_pressed is dictionary with following structure:
+# keys_pressed = {'KeyName': <Pressed or not(boolean)>}
+keys_pressed = {'SPACE': False, 'Akey': False, 'Skey': False, 'Dkey': False, 'Wkey': False}
+
+
+def handle_events(events):
+    '''
+    function, which is aimed at managing keyboard(or mouse) events and proper responses for them
+    '''
+
+    global finished, keys_pressed
+
+    for event in events:
+        if event.type == pg.QUIT:
+            finished = True
+        elif event.type == pg.KEYDOWN:
+            if event.key == pg.K_SPACE:
+                if keys_pressed['SPACE']:
+                    keys_pressed['SPACE'] = True
+                else:
+                    keys_pressed['SPACE'] = False
+            if event.key == pg.K_a:
+                keys_pressed['Akey'] = True
+            if event.key == pg.K_s:
+                keys_pressed['Skey'] = True
+            if event.key == pg.K_d:
+                keys_pressed['Dkey'] = True
+            if event.key == pg.K_w:
+                keys_pressed['Wkey'] = True
+        elif event.type == pg.KEYUP:
+            if event.key == pg.K_a:
+                keys_pressed['Akey'] = False
+            if event.key == pg.K_s:
+                keys_pressed['Skey'] = False
+            if event.key == pg.K_d:
+                keys_pressed['Dkey'] = False
+            if event.key == pg.K_w:
+                keys_pressed['Wkey'] = False
+
+    hero.move(objects, keys_pressed['Akey'], keys_pressed['Wkey'], keys_pressed['Skey'], keys_pressed['Dkey'])
 
 
 while not finished:
@@ -41,31 +77,5 @@ while not finished:
     pg.display.update()
     # Reaction to keys
     clock.tick(FPS)
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            finished = True
-        elif event.type == pg.KEYDOWN:
-            if (event.key == pg.K_SPACE):
-                if (Kspace):
-                    Kspace = 0
-                else:
-                    Kspace = 1
-            if event.key == pg.K_a:
-                Akey = 1
-            if event.key == pg.K_s:
-                Skey = 1
-            if event.key == pg.K_d:
-                Dkey = 1
-            if event.key == pg.K_w:
-                Wkey = 1
-        elif event.type == pg.KEYUP:
-            if event.key == pg.K_a:
-                Akey = 0
-            if event.key == pg.K_s:
-                Skey = 0
-            if event.key == pg.K_d:
-                Dkey = 0
-            if event.key == pg.K_w:
-                Wkey = 0
 
-    hero.move(objects, Akey, Wkey, Skey, Dkey)
+    handle_events(pg.event.get())
