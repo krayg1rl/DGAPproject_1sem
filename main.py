224f9da8cc3f1128a90ea628dbac577c08aca3a4
Daniil-Kharitonov <prostoharia@gmail.com>
2023-12-14 13:37:53 +0300

    added menu file which contains classes and methods for UI. Now there's only Button class here. Implemented menu system into the game

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ the main loop should be here
 """
 
 from object import *
+import menu
 import pygame as pg
 import time
 
@@ -15,10 +16,18 @@ TIME_LIMIT = 120  # In seconds
 pg.init()
 screen = pg.display.set_mode((WIDTH, HEIGHT))
 background = pg.transform.scale(pg.image.load("pictures/map.png"), (WIDTH, HEIGHT))
+# TODO
+# add image for main menu
+menu_background = pg.transform.scale(pg.image.load("pictures/map.png"), (WIDTH, HEIGHT))
 desk_image = pg.transform.scale(pg.image.load("pictures/Desk.png"), (170, 110))
 prep_image = pg.transform.scale(pg.image.load("pictures/prep2.png"), (100, 125))
-
 scanner_image = pg.transform.scale(pg.image.load("pictures/radar.png"), size=(340, 250))
+
+# load button images
+options_img = pg.image.load("pictures/button_options.png").convert_alpha()
+
+# initialiasating buttons
+options_button = menu.Button(297, 250, options_img, 1)
 
 clock = pg.time.Clock()
 
@@ -47,6 +56,7 @@ start_time = pg.time.get_ticks()
 time_left = TIME_LIMIT
 
 finished = False
+menu_state = 'main'
 
 hero = Main_character(screen)
 hero.speed.y = 30
@@ -96,6 +106,7 @@ def handle_events(events):
     hero.move(physical_objects, keys_pressed['Akey'], keys_pressed['Wkey'], keys_pressed['Skey'], keys_pressed['Dkey'])
     npc.move()
 
+
 def timer():
     global start_time, time_left
 
@@ -113,27 +124,39 @@ def timer():
         time_left_seconds = '0' + time_left_seconds
     if len(time_left_minutes) == 1:
         time_left_minutes = '0' + time_left_minutes
+    if len(time_left_milliseconds) == 1:
+        time_left_milliseconds = '0' + time_left_milliseconds
 
     time_passed = timer_font.render('Timer: ' + time_left_minutes + ':' + time_left_seconds + ':' + time_left_milliseconds, True, (255, 255, 255, 255))
 
-    screen.blit(time_passed, (180, 53))
+    screen.blit(time_passed, (WIDTH / 7.11, HEIGHT / 13.585))
 
 
 while not finished:
     # TODO
-    #drawing of objects should be here
-    screen.blit(background, (0, 0))
+    # drawing of objects should be here
 
-    timer()
-    hero_point = str(hero.points)
-    points = points_font.render(hero_point, True, (255, 255, 255, 255))
-    screen.blit(points, (1000, 53))
-
-    for obj in visible_objects:
-        obj.draw()
-    hero.draw()
-    pg.display.update()
-    # Reaction to keys
     clock.tick(FPS)
 
+    if menu_state == 'game':
+        screen.blit(background, (0, 0))
+
+        timer()
+
+        hero_point = str(hero.points)
+        points = points_font.render(hero_point, True, (255, 255, 255, 255))
+        screen.blit(points, (1000, 53))
+
+        for obj in visible_objects:
+            obj.draw()
+        hero.draw()
+
+    if menu_state == 'main':
+        screen.blit(menu_background, (0, 0))
+
+        if options_button.draw(screen):
+            menu_state = "game"
+
     handle_events(pg.event.get())
+
+    pg.display.update()
